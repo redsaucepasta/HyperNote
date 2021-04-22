16032ad741da878c6418be6684ecc7c050541e3a
Amit Dethe <26amitdethe@gmail.com>
2021-04-22 09:16:06 +0530

    Initial Commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Note = () => {
+  return (
+    <div className="note">
+      <h1>Title</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores,
+        expedita nulla ipsam tempora adipisci quisquam ut. Quam velit unde,
+        provident, nisi vel rem ipsa nostrum porro modi fuga repudiandae omnis.
+      </p>
+    </div>
+  );
+};
+
+export default Note;
