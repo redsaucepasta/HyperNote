f6e6c77375d8b3414460b70e5410998afefc3743
Amit Dethe <26amitdethe@gmail.com>
2021-05-19 12:41:44 +0530

    Bug fixes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,7 @@ import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import CreateArea from "./CreateArea"
-
-
-
+import CreateArea from "./CreateArea";
 const App = () => {
   const [notes, setNotes] = useState([]);
 
@@ -20,19 +17,27 @@ const App = () => {
       return prevNotes.filter((noteItem, index) => {
         return index !== id;
       });
-    })
+    });
   }
 
-  return (<div>
-    <Header/>
-  <CreateArea onAdd={addNote} />
-     {
-      notes.map((note, index) => {
-        return (<Note key={index} id={index} title={note.title} content={note.content} onDelete={deleteNote}/>);
-      })
-    }
-    <Footer/>
-  </div>);
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((note, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={note.title}
+            content={note.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
